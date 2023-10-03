e30bcb8e3da2781ea4a99c2ae9df00fe3ea91ea8
Ayush Verma <tgmcodeyogi@gmail.com>
2023-10-03 11:32:41 +0530

    Modified 2

--- src/redux/actions/individualShow.ts
+++ src/redux/actions/individualShow.ts
@@ -1,18 +1,18 @@
-// import show from "../../models/show";
-// import { ActionCreator } from "./index";
+import show from "../../models/show";
+import { ActionCreator } from "./index";
 
-// export const GET_INDIVIDUAL_SHOW = "GET_INDIVIDUAL_SHOW";
+export const GET_INDIVIDUAL_SHOW = "GET_INDIVIDUAL_SHOW";
 
-// export const individualShowAction: ActionCreator<show> = (show: show) => ({
-//   type: GET_INDIVIDUAL_SHOW,
-//   payload: show,
-// });
+export const individualShowAction: ActionCreator<show> = (show: show) => ({
+  type: GET_INDIVIDUAL_SHOW,
+  payload: show,
+});
 
-// export const LOAD_INDIVIDUAL_SHOW = "LOAD_INDIVIDUAL_SHOW";
+export const LOAD_INDIVIDUAL_SHOW = "LOAD_INDIVIDUAL_SHOW";
 
-// export const loadIndividualShowAction: ActionCreator<number> = (
-//   showId: number
-// ) => ({
-//   type: LOAD_INDIVIDUAL_SHOW,
-//   payload: showId,
-// });
+export const loadIndividualShowAction: ActionCreator<number> = (
+  showId: number
+) => ({
+  type: LOAD_INDIVIDUAL_SHOW,
+  payload: showId,
+});
